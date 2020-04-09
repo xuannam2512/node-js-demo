14a1d7cdba09a41fbbb860a4149e3e0ef1bc5eec
namlemorphotech <nam.le@morphotech.co.uk>
2020-04-09 12:24:39 +0700

    change port

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
     res.send('hello world')
 })
   
-app.listen(3000, () => {
-    console.log("Listening on port 3000");
+app.listen(8080, () => {
+    console.log("Listening on port 8080");
 });
